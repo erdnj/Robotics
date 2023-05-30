c07e979c6f06f371230f3c0202240c9cc9960221
erdnj <erdnjj@gmail.com>
2023-05-31 02:23:37 +0300

    Fork step-in out implemented, moving parameters improved

---
+++ autonomous_transpalet_navigation/action.py
@@ -0,0 +1,228 @@
+import numpy as np
+from enum import Enum
+
+from rrt_star import get_rrt_star_path
+
+States = Enum('State', ['Lift_Up', 'Lift_Down', 'Fork_Follow', 'Head_Follow', 'Head_Turn', 'Fork_Adjust', 'Fork_Turn',
+                        'Fork_Step_In', 'Fork_Step_Out', 'Path_Plan', 'End'])
+
+
+class Action:
+    def __init__(self, data, wall_info_list, num_task):
+        self.drive_speed_actuator = data.actuator("drive_speed")
+        self.steering_angle_actuator = data.actuator("steering_angle")
+        self.fork_height_actuator = data.actuator("fork_height")
+
+        self.robot_link = data.site("base_link")
+        self.front_robot_link = data.site("front_base_link")
+        self.palet_link = None
+        self.drop_link = None
+        self.wall_list = wall_info_list
+        self.task_list = [(data.site("palet_link{}".format(i)),
+                           data.site("palet_drop{}".format(i))) for i in range(1, num_task + 1)]
+        self.currentTaskId = 0
+        self.state = States.Path_Plan
+        self.isWeighted = False
+        self.currentPath = None
+        self.currentLine = None
+
+        # path planning vars
+        self.reference_path = None
+
+        # goal related
+        self.goal_line = None
+
+        # Steer Logic Related Vars
+        self.maxNextPathId = 0
+        self.currentPathId = 0
+        self.currentLine = None
+        self.nextLine = None
+        self.referencedNextLine = None
+
+        # steer parameters
+        self.par_next_dist = -0.2 #0.0001
+        self.par_line = -1.0
+        self.par_direction = -1.0
+
+    def forward(self):
+        match self.state:
+            case States.Lift_Up:
+                self.lift_up()
+            case States.Lift_Down:
+                self.lift_down()
+            case States.Fork_Follow:
+                self.fork_follow()
+            case States.Head_Follow:
+                self.head_follow()
+            case States.Head_Turn:
+                self.head_turn()
+            case States.Fork_Adjust:
+                self.fork_adjust()
+            case States.Fork_Turn:
+                self.fork_turn()
+            case States.Fork_Step_In:
+                self.fork_step_in()
+            case States.Fork_Step_Out:
+                self.fork_step_out()
+            case States.Path_Plan:
+                self.find_path()
+            case States.End:
+                None
+
+    def find_path(self, extra_obs_list=None):
+        goal_link = self.task_list[self.currentTaskId][1] if self.isWeighted else self.task_list[self.currentTaskId][0]
+
+        obs_list = self.wall_list
+        if extra_obs_list is not None:
+            np.concatenate((obs_list, extra_obs_list), axis=0)
+
+        start_point = self.front_robot_link.xpos[0:2] if self.isWeighted else self.robot_link.xpos[0:2]
+        start_ori = - self.front_robot_link.xmat[[0, 3]] if self.isWeighted else self.front_robot_link.xmat[[0, 3]]
+
+        goal_point, goal_ori = get_goal_point(goal_link)
+        reference_path_2column = get_rrt_star_path(start_point, goal_point, self.wall_list)
+        self.reference_path = np.pad(reference_path_2column, ((0, 0), (0, 1)), 'constant', constant_values=(0, 0))
+        print(self.reference_path)
+        # after found path set reference_path
+        # Steer Logic Related Vars
+        self.maxNextPathId = self.reference_path.shape[0] - 2
+        self.currentPathId = 0
+        self.currentLine = line_formula(self.reference_path[0], self.reference_path[1])
+        if len(self.reference_path) > 2:
+            self.nextLine = line_formula(self.reference_path[1], self.reference_path[2])
+            self.referencedNextLine = referenced_line(self.reference_path[0], self.nextLine[0])
+        self.state = States.Head_Follow if self.isWeighted else States.Fork_Follow
+
+    def path_follow(self):
+        cur_pos = self.front_robot_link.xpos[0:2] if self.state == States.Head_Follow \
+            else self.robot_link.xpos[0:2]
+        direction = - self.front_robot_link.xmat[[0, 3]] if self.state == States.Head_Follow \
+            else self.front_robot_link.xmat[[0, 3]]
+
+        # dotProduct = np.dot(direction, currentLine[1])
+        cross_product = np.cross(direction, self.currentLine[1])
+
+        dist_to_line = line_distance(cur_pos=cur_pos, current_line=self.currentLine[0])
+        new_steer = dist_to_line * self.par_line + cross_product * self.par_direction
+        self.steering_angle_actuator.ctrl = np.clip(new_steer, -1, 1)
+
+        # transfer logic for next line
+        if self.nextLine is not None:  # if there is a nextline
+            if line_distance(cur_pos=cur_pos, current_line=self.referencedNextLine) > self.par_next_dist:
+                self.currentPathId += 1
+                self.currentLine = self.nextLine
+                if self.currentPathId != self.maxNextPathId:
+                    self.nextLine = line_formula(self.reference_path[self.currentPathId + 1],
+                                                 self.reference_path[self.currentPathId + 2])
+                    self.referencedNextLine = referenced_line(self.reference_path[self.currentPathId], self.nextLine[0])
+                else:
+                    self.nextLine = None
+
+    def fork_follow(self):
+        self.par_line = 2
+        self.par_direction = 5
+        self.drive_speed_actuator.ctrl = -1
+        self.path_follow()
+
+    def head_follow(self):
+        self.par_line = -1.5
+        self.par_direction = -1.0
+        self.drive_speed_actuator.ctrl = 1
+        self.path_follow()
+
+    def head_turn(self):
+        None
+
+    def fork_adjust(self):
+        None
+
+    def fork_turn(self):
+        None
+
+    def fork_step_in(self):
+        self.drive_speed_actuator.ctrl = -1
+        self.steering_angle_actuator.ctrl = 0
+        direction = self.front_robot_link.xmat[[0, 3]]
+        goal_link = self.task_list[self.currentTaskId][1] if self.isWeighted else self.task_list[self.currentTaskId][0]
+        self.goal_line = line_formula_from_direction(goal_link.xpos[0:2], direction)
+
+        cur_pos = self.robot_link.xpos[0:2]
+        distance = line_distance(cur_pos, self.goal_line)
+        if distance < -0.15:
+            self.drive_speed_actuator.ctrl = 0
+            self.state = States.Lift_Down if self.isWeighted else States.Lift_Up
+
+    def lift_up(self):
+        if self.fork_height_actuator.ctrl < 1:
+            self.fork_height_actuator.ctrl += 0.05
+        else:
+            self.isWeighted = True
+            self.state = States.Path_Plan
+
+    def lift_down(self):
+        if self.fork_height_actuator.ctrl > 0:
+            self.fork_height_actuator.ctrl -= 0.05
+        else:
+            self.isWeighted = False
+            self.state = States.Fork_Step_Out
+
+    def fork_step_out(self):
+        self.drive_speed_actuator.ctrl = 1
+        self.steering_angle_actuator.ctrl = 0
+
+        direction = self.front_robot_link.xmat[[0, 3]]
+        goal_link = self.task_list[self.currentTaskId][0]
+        self.goal_line = line_formula_from_direction(goal_link.xpos[0:2], direction)
+
+        cur_pos = self.robot_link.xpos[0:2]
+        distance = line_distance(cur_pos, self.goal_line)
+        if distance > 0.8:
+            self.drive_speed_actuator.ctrl = 0
+
+            if len(self.task_list) == self.currentTaskId + 1:
+                self.state = States.End
+            else:
+                self.currentTaskId += 1
+                self.state = States.Path_Plan
+
+
+def get_goal_point(goal_link):
+    pos = goal_link.xpos[0:2]
+    direction = goal_link.xmat[[0, 3]]
+    unit_direction = direction / np.linalg.norm(direction)
+    return pos + unit_direction * 0.8, -direction
+
+
+def line_formula(from_point, to_point):
+    vector = np.array([to_point[0] - from_point[0], to_point[1] - from_point[1]])
+    return np.array([
+        from_point[1] - to_point[1],  # y0 - y1
+        to_point[0] - from_point[0],  # x1 - x0
+        from_point[0] * to_point[1],  # x0*y1
+        - from_point[1] * to_point[0]  # x1*y0
+    ]), vector / np.linalg.norm(vector)
+
+
+def line_formula_from_direction(point, direction):
+    return np.array([
+        direction[0],  # xd
+        direction[1],  # yd
+        - point[0] * direction[0],  # -xd*x0
+        - point[1] * direction[1]  # -yd*y0
+    ])
+
+
+# got distance a point from a line
+def line_distance(cur_pos, current_line):
+    padded_pos = np.pad(cur_pos[0:2], (0, 2), constant_values=(1, 1)).reshape(4, 1)
+    distance = np.matmul(current_line, padded_pos)[0]
+    return distance
+
+
+# To got positive distance after surpass the next line
+def referenced_line(reference_pos, current_line):
+    distance = line_distance(reference_pos, current_line)
+    if distance < 0:
+        return current_line
+    else:
+        return -current_line
--- autonomous_transpalet_navigation/simulation.py
+++ autonomous_transpalet_navigation/simulation.py
@@ -2,7 +2,10 @@ import mujoco
 import mujoco_viewer
 import numpy
 import math
+
+import numpy as np
 from scipy.spatial.transform import Rotation
+from action import Action
 
 model = mujoco.MjModel.from_xml_path('../models/atlas_transpalet/scene.xml')
 data = mujoco.MjData(model)
@@ -16,11 +19,12 @@ steering_angle_actuator = data.actuator("steering_angle")
 fork_height_actuator = data.actuator("fork_height")
 
 robot_base_link = data.site("base_link")
-palet_link = data.site("palet_link")
-palet_drop = data.site("palet_drop")
+palet_link = data.site("palet_link1")
+palet_drop = data.site("palet_drop1")
 
 wall_number = 12
 wall_information = []
+obs_info = []
 
 for wall_index in range(1, wall_number + 1):
     wall = model.geom('wall_' + str(wall_index))
@@ -32,94 +36,138 @@ for wall_index in range(1, wall_number + 1):
     wall_corners = [[wall_bottom_left_x, wall_bottom_left_y], [wall_top_right_x, wall_top_right_y]]
     wall_information.append(wall_corners)
 
-print(wall_information)
+for wall_index in range(1, wall_number + 1):
+    wall = model.geom('wall_' + str(wall_index))
+    if wall.size[0] > wall.size[1]:
+        wall_radius = wall.size[1] / 2
+        start_x = (wall.pos[0] - wall.size[0]) + wall_radius
+        y = wall.pos[1]
+        end_x = (wall.pos[0] + wall.size[0]) - wall_radius
+        for i in np.arange(start_x, end_x, 0.1):
+            obs_info.append((i, y, wall_radius))
+    else:
+        wall_radius = wall.size[0]/2
+        start_y = (wall.pos[1] - wall.size[1]) + wall_radius
+        x = wall.pos[0]
+        end_y = (wall.pos[1] + wall.size[1]) - wall_radius
+        for i in np.arange(start_y, end_y, 0.1):
+            obs_info.append((x, i, wall_radius))
+mujoco.mj_step(model, data)
 
-for _ in range(4000):
+actioner = Action(data, obs_info, 1)
+
+for _ in range(40000):
     previous_time = data.time
 
-    while ((data.time - previous_time) < 1.0 / 60):
+    while (data.time - previous_time) < 1.0 / 60:
         mujoco.mj_step(model, data)
 
     if viewer.is_alive:
-        if (state == 0):
-            forklift_rotation_matrix = numpy.array(robot_base_link.xmat).reshape(3, 3)
-            forklift_rotation = Rotation.from_matrix(forklift_rotation_matrix)
-            forklift_rotation_angles = forklift_rotation.as_euler("zyx", degrees=True)
-
-            if (abs(forklift_rotation_angles[0] + 90) < 1):
-                drive_speed_actuator.ctrl = 0.0
-                steering_angle_actuator.ctrl = 0.0
-                state = state + 1
-                state_end_time = data.time
-            else:
-                drive_speed_actuator.ctrl = 1.0
-                steering_angle_actuator.ctrl = -1.0
-
-        elif (state == 1):
-            drive_speed_actuator.ctrl = 0.0
-            steering_angle_actuator.ctrl = 0.0
-
-            if (abs(state_end_time - data.time) > 2):
-                state = state + 1
-                state_end_time = data.time
-
-        elif (state == 2):
-            forklift_position = robot_base_link.xpos
-            palet_position = palet_link.xpos
-
-            if (abs(forklift_position[1] - palet_position[1]) < 0.01):
-                drive_speed_actuator.ctrl = 0.0
-                steering_angle_actuator.ctrl = 0.0
-                state = state + 1
-            else:
-                drive_speed_actuator.ctrl = -1 * abs(forklift_position[1] - palet_position[1])
-                steering_angle_actuator.ctrl = 0.0
-
-        elif (state == 3):
-            forklift_rotation_matrix = numpy.array(robot_base_link.xmat).reshape(3, 3)
-            forklift_rotation = Rotation.from_matrix(forklift_rotation_matrix)
-            forklift_rotation_angles = forklift_rotation.as_euler("zyx", degrees=True)
-
-            if (abs(forklift_rotation_angles[0] - 0) < 1):
-                drive_speed_actuator.ctrl = 0.0
-                steering_angle_actuator.ctrl = 0.0
-                state = state + 1
-                state_end_time = data.time
-            else:
-                drive_speed_actuator.ctrl = 1.0
-                steering_angle_actuator.ctrl = 1.0
-
-        elif (state == 4):
-            forklift_position = robot_base_link.xpos
-            palet_position = palet_link.xpos
-
-            if (abs(forklift_position[0] - (palet_position[0] - 0.25)) < 0.1):
-                drive_speed_actuator.ctrl = 0.0
-                steering_angle_actuator.ctrl = 0.0
-                state = state + 1
-                state_end_time = data.time
-
-            else:
-                drive_speed_actuator.ctrl = (-1 * abs(forklift_position[0] - (palet_position[0] - 0.25)) / 2)
-                steering_angle_actuator.ctrl = 0.0
-
-        elif (state == 5):
-            drive_speed_actuator.ctrl = 0.0
-            steering_angle_actuator.ctrl = 0.0
-
-            if (abs(state_end_time - data.time) > 2):
-                state = state + 1
-                state_end_time = data.time
-            else:
-                fork_height_actuator.ctrl = abs(state_end_time - data.time)
-
-        elif (state == 6):
-            drive_speed_actuator.ctrl = 0.60
-            steering_angle_actuator.ctrl = 0.8
-            fork_height_actuator.ctrl = 1.0
-
+        result = actioner.forward()
+        if result == -1:
+            break
+        if actioner.reference_path is not None:
+            reference_path = actioner.reference_path
+            for i in range(1, reference_path.shape[0]):
+                path_vector = (reference_path[i] - reference_path[i - 1])
+                path_step_vector = path_vector * 0.2 / np.linalg.norm(path_vector)
+                current_dot = reference_path[i - 1]
+                step_size = np.linalg.norm(path_vector) / 0.2
+                for _ in range(round(step_size)):
+                    viewer.add_marker(
+                        pos=current_dot,
+                        size=[0.02, 0.02, 0.01],
+                        rgba=[1, 1, 0, 0.7],
+                        type=mujoco.mjtGeom.mjGEOM_SPHERE,
+                    )
+                    current_dot = current_dot + path_step_vector
+        viewer.add_marker(
+            pos=[*data.site("base_link").xpos[0:2],0],
+            size=[0.02, 0.02, 0.01],
+            rgba=[1, 0, 0, 0.7],
+            type=mujoco.mjtGeom.mjGEOM_SPHERE,
+        )
         viewer.render()
-
+        # if state == 0:
+        #     forklift_rotation_matrix = numpy.array(robot_base_link.xmat).reshape(3, 3)
+        #     forklift_rotation = Rotation.from_matrix(forklift_rotation_matrix)
+        #     forklift_rotation_angles = forklift_rotation.as_euler("zyx", degrees=True)
+        #
+        #     if abs(forklift_rotation_angles[0] + 90) < 1:
+        #         drive_speed_actuator.ctrl = 0.0
+        #         steering_angle_actuator.ctrl = 0.0
+        #         state = state + 1
+        #         state_end_time = data.time
+        #     else:
+        #         drive_speed_actuator.ctrl = 1.0
+        #         steering_angle_actuator.ctrl = -1.0
+        #
+        # elif state == 1:
+        #     drive_speed_actuator.ctrl = 0.0
+        #     steering_angle_actuator.ctrl = 0.0
+        #
+        #     if abs(state_end_time - data.time) > 2:
+        #         state = state + 1
+        #         state_end_time = data.time
+        #
+        # elif state == 2:
+        #     forklift_position = robot_base_link.xpos
+        #     palet_position = palet_link.xpos
+        #
+        #     if abs(forklift_position[1] - palet_position[1]) < 0.01:
+        #         drive_speed_actuator.ctrl = 0.0
+        #         steering_angle_actuator.ctrl = 0.0
+        #         state = state + 1
+        #     else:
+        #         drive_speed_actuator.ctrl = -1 * abs(forklift_position[1] - palet_position[1])
+        #         steering_angle_actuator.ctrl = 0.0
+        #
+        # elif state == 3:
+        #     forklift_rotation_matrix = numpy.array(robot_base_link.xmat).reshape(3, 3)
+        #     forklift_rotation = Rotation.from_matrix(forklift_rotation_matrix)
+        #     forklift_rotation_angles = forklift_rotation.as_euler("zyx", degrees=True)
+        #     forklift_position = robot_base_link.xpos
+        #
+        #     if abs(forklift_rotation_angles[0] - 0) < 1:
+        #         drive_speed_actuator.ctrl = 0.0
+        #         steering_angle_actuator.ctrl = 0.0
+        #         state = state + 1
+        #         state_end_time = data.time
+        #     else:
+        #         drive_speed_actuator.ctrl = 1.0
+        #         steering_angle_actuator.ctrl = 1.0
+        #
+        # elif state == 4:
+        #     forklift_position = robot_base_link.xpos
+        #     palet_position = palet_link.xpos
+        #
+        #     if abs(forklift_position[0] - (palet_position[0] - 0.25)) < 0.1:
+        #         drive_speed_actuator.ctrl = 0.0
+        #         steering_angle_actuator.ctrl = 0.0
+        #         state = state + 1
+        #         state_end_time = data.time
+        #
+        #     else:
+        #         drive_speed_actuator.ctrl = (-1 * abs(forklift_position[0] - (palet_position[0] - 0.25)) / 2)
+        #         steering_angle_actuator.ctrl = 0.0
+        #
+        # elif state == 5:
+        #     drive_speed_actuator.ctrl = 0.0
+        #     steering_angle_actuator.ctrl = 0.0
+        #
+        #     if abs(state_end_time - data.time) > 2:
+        #         state = state + 1
+        #         state_end_time = data.time
+        #     else:
+        #         fork_height_actuator.ctrl = abs(state_end_time - data.time)
+        #
+        # elif state == 6:
+        #     drive_speed_actuator.ctrl = 0.60
+        #     steering_angle_actuator.ctrl = 0.8
+        #     fork_height_actuator.ctrl = 1.0
+        #
+        # viewer.render()
+        # print(state)
     else:
         break
 
